ec7bb0ab2f74ead53abfc41d9a8f0529bf3c95c3
vianchr <143708027+vianchr@users.noreply.github.com>
2024-05-28 22:49:35 +0700

    perbaikan

--- tugas_2.py
+++ tugas_2.py
@@ -3,10 +3,12 @@ def palindrom(huruf,awal=0,akhir=0):
     if akhir==0:
         akhir=len(huruf)-1
     if huruf[awal] != huruf[akhir]:
-        return ("bukan palindrom")
+        return (f"{huruf} bukan palindrom")
     if awal >= akhir:
-        return ("palindrom")
+        return (f"{huruf} palindrom")
     else:
         return palindrom(huruf,awal+1,akhir-1)
 
 print(palindrom("kak"))
+print(palindrom("khukhu"))
+print(palindrom("popoopop"))
